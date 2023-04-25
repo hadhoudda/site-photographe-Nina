d88a3d7800cca7c664afc89343c8e6db38c77aeb
Ben Abdallah Houda <ahhouda@yahoo.fr>
2023-04-25 07:07:59 +0200

    debugue

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,8 +154,8 @@
         }
       });
       next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
+        imagesCollection[index -1] ||
+        imagesCollection[imagesCollection.length];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     nextImage() {
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index +1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
